6fda5b685bf15cf8955ed52076a2ccc98d47b3e9
Michael Bonner <michaelwbonner@gmail.com>
2022-05-09 15:26:51 -0600

    new favicon

--- pages/index.js
+++ pages/index.js
@@ -74,101 +74,104 @@ export default function Home() {
     <div>
       <Head>
         <title>Tesseract Test</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
 
       <main className="mx-auto py-12 grid gap-y-8 px-4 lg:px-8">
         <h1 className="text-3xl">Tesseract Test</h1>
 
         <div className="grid lg:grid-cols-2 rounded border">
-          <div className="py-4 px-8 grid gap-y-8">
-            <div className="flex flex-wrap items-center gap-4">
-              <label
-                className="text-base font-medium text-gray-700 whitespace-nowrap"
-                htmlFor="file"
-              >
-                Upload image
-              </label>
-              <input
-                accept="image/png, image/jpeg"
-                type="file"
-                id="file"
-                onChange={(event) => {
-                  if (
-                    event.currentTarget.files[0] &&
-                    (event.currentTarget.files[0].type === "image/jpeg" ||
-                      event.currentTarget.files[0].type === "image/png")
-                  ) {
-                    setUploadedFile(event.target.files[0]);
-                    getUrlFromFile(event.target.files[0]);
-                  } else {
-                    alert("Unsupported file type");
-                  }
-                }}
-              />
-            </div>
-            <div
-              className={`w-full transition-opacity ${
-                tesseractStatus === "working" || tesseractStatus === "setup"
-                  ? "opacity-100"
-                  : "opacity-0"
-              }`}
-            >
-              <div className="flex justify-between mb-1">
-                <span className="text-base font-medium text-gray-700">
-                  Processing image
-                </span>
-                <span className="text-sm font-medium text-gray-700">{`${Math.ceil(
-                  progress * 100
-                )}%`}</span>
-              </div>
-              <div className="w-full bg-gray-200 rounded-full h-2.5">
-                <div
-                  className="bg-gray-600 h-2.5 rounded-full"
-                  style={{ width: `${Math.ceil(progress * 100)}%` }}
-                ></div>
-              </div>
-            </div>
-            {uploadedFileDisplayUrl && (
-              <div className="relative">
-                {(tesseractStatus === "working" ||
-                  tesseractStatus === "setup") && (
-                  <div className="absolute inset-0 flex justify-center items-center bg-white bg-opacity-50">
-                    <span
-                      className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-gray-500 bg-opacity-100"
-                      disabled=""
-                    >
-                      <svg
-                        className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                      >
-                        <circle
-                          className="opacity-25"
-                          cx="12"
-                          cy="12"
-                          r="10"
-                          stroke="currentColor"
-                          strokeWidth="4"
-                        ></circle>
-                        <path
-                          className="opacity-75"
-                          fill="currentColor"
-                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                        ></path>
-                      </svg>
-                      Processing...
-                    </span>
-                  </div>
-                )}
-                <img
-                  alt="Uploaded file"
-                  className="m-auto max-h-[70vh]"
-                  src={uploadedFileDisplayUrl}
+          <div>
+            <div className="py-4 px-8 grid gap-y-8">
+              <div className="flex flex-wrap items-center gap-4">
+                <label
+                  className="text-base font-medium text-gray-700 whitespace-nowrap"
+                  htmlFor="file"
+                >
+                  Upload image
+                </label>
+                <input
+                  accept="image/png, image/jpeg"
+                  type="file"
+                  id="file"
+                  onChange={(event) => {
+                    if (
+                      event.currentTarget.files[0] &&
+                      (event.currentTarget.files[0].type === "image/jpeg" ||
+                        event.currentTarget.files[0].type === "image/png")
+                    ) {
+                      setUploadedFile(event.target.files[0]);
+                      getUrlFromFile(event.target.files[0]);
+                    } else {
+                      alert("Unsupported file type");
+                    }
+                  }}
                 />
               </div>
-            )}
+              <div
+                className={`w-full transition-opacity ${
+                  tesseractStatus === "working" || tesseractStatus === "setup"
+                    ? "opacity-100"
+                    : "opacity-0"
+                }`}
+              >
+                <div className="flex justify-between mb-1">
+                  <span className="text-base font-medium text-gray-700">
+                    Processing image
+                  </span>
+                  <span className="text-sm font-medium text-gray-700">{`${Math.ceil(
+                    progress * 100
+                  )}%`}</span>
+                </div>
+                <div className="w-full bg-gray-200 rounded-full h-2.5">
+                  <div
+                    className="bg-gray-600 h-2.5 rounded-full"
+                    style={{ width: `${Math.ceil(progress * 100)}%` }}
+                  ></div>
+                </div>
+              </div>
+              {uploadedFileDisplayUrl && (
+                <div className="relative">
+                  {(tesseractStatus === "working" ||
+                    tesseractStatus === "setup") && (
+                    <div className="absolute inset-0 flex justify-center items-center bg-white bg-opacity-50">
+                      <span
+                        className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-gray-500 bg-opacity-100"
+                        disabled=""
+                      >
+                        <svg
+                          className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <circle
+                            className="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            strokeWidth="4"
+                          ></circle>
+                          <path
+                            className="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                          ></path>
+                        </svg>
+                        Processing...
+                      </span>
+                    </div>
+                  )}
+                  <img
+                    alt="Uploaded file"
+                    className="m-auto max-h-[70vh]"
+                    src={uploadedFileDisplayUrl}
+                  />
+                </div>
+              )}
+            </div>
           </div>
           <div
             className={`py-4 px-8 bg-gray-100 border-gray-200 rounded whitespace-pre-line`}
