a2613430fc05c5556e07da8bdd33ab174873533c
Michael Bonner <michaelwbonner@gmail.com>
2022-05-09 10:56:59 -0600

    cspell config

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
-import Image from "next/image";
 import { useEffect, useState } from "react";
 import { createWorker } from "tesseract.js";
 
