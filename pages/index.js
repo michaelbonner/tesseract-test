c6767e04a4883a0b4ef4238d01b4056fac954e8e
Michael Bonner <michaelwbonner@gmail.com>
2022-05-09 10:32:11 -0600

    more styling

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,14 @@ import { createWorker } from "tesseract.js";
 
 export default function Home() {
   const [progress, setProgress] = useState(0);
-  const [text, setText] = useState("processing...");
+  const [text, setText] = useState("");
 
   useEffect(() => {
     const worker = createWorker({
       logger: (workerData) => {
-        setProgress(workerData?.progress || 0);
+        if (workerData.status === "recognizing text") {
+          setProgress(workerData?.progress || 0);
+        }
       },
     });
 
@@ -37,31 +39,38 @@ export default function Home() {
       <main className="max-w-7xl mx-auto py-12 grid gap-y-8 px-4 lg:px-8">
         <h1 className="text-3xl">Tesseract Test</h1>
 
-        <div className="w-full max-w-md">
-          <label className="block w-full" htmlFor="processing">
-            Processing image progress:
-          </label>
-          <progress
-            className="block w-full"
-            id="processing"
-            value={progress}
-            max="1"
+        {progress < 1 && (
+          <div className={`w-full max-w-md`}>
+            <label className="block w-full" htmlFor="processing">
+              Processing image progress:
+            </label>
+            <progress
+              className="block w-full"
+              id="processing"
+              value={progress}
+              max="1"
+            >
+              {Math.ceil(progress * 100)}%{" "}
+            </progress>
+          </div>
+        )}
+
+        <div className="grid lg:grid-cols-2 rounded border">
+          <div>
+            <Image
+              src="/screen-shot.png"
+              width="1460"
+              height="1288"
+              alt="Shape Up book screenshot"
+            />
+          </div>
+          <div
+            className={`py-4 px-8 bg-gray-100 border-gray-200 rounded whitespace-pre-line ${
+              !text && "animate-pulse"
+            }`}
           >
-            {Math.ceil(progress * 100)}%{" "}
-          </progress>
-        </div>
-
-        <div className="py-4 px-8 bg-gray-100 border-gray-200 rounded whitespace-pre-line">
-          {text}
-        </div>
-
-        <div className="py-4 px-8 bg-gray-200">
-          <Image
-            src="/screen-shot.png"
-            width="1460"
-            height="1288"
-            alt="Shape Up book screenshot"
-          />
+            {text || "Processing..."}
+          </div>
         </div>
       </main>
     </div>
