6b741a0425f732a1fb812c7b9d50b7da4b244a0c
Michael Bonner <michaelwbonner@gmail.com>
2022-08-24 16:59:20 -0600

    fix layout off page on mobile

--- pages/index.js
+++ pages/index.js
@@ -74,6 +74,7 @@ export default function Home() {
                   Upload image
                 </label>
                 <input
+                  className="w-full"
                   accept="image/png, image/jpeg"
                   type="file"
                   id="file"
