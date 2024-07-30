0773cc9e1fe7338d877f71254e577933ccbec58b
Michael Bonner <michaelwbonner@gmail.com>
2024-07-29 20:11:10 -0600

    Do not use babel-plugin-react-compiler

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    reactCompiler: true,
-  },
+  experimental: {},
   i18n: {
     locales: ["en-US"],
     defaultLocale: "en-US",
